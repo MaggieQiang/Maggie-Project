365c38c54b07246efa44a28ca2de127b2ef6d7c0
MaggieQiang <maggieqiang717@gmail.com>
2025-10-15 23:11:45 -0700

    Animation (Daehyun and Pink Monster)

--- Assets/Scenes/Scripts/Player.cs
+++ Assets/Scenes/Scripts/Player.cs
@@ -1,3 +1,5 @@
+using UnityEditor.Animations;
+using UnityEditor.Callbacks;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,18 +8,24 @@ public class Player : MonoBehaviour
     float movementX;
     float movementY;
     [SerializeField] float speed = 6f;
-
     [SerializeField] Rigidbody2D rb;
+    [SerializeField] float jumpPower = 5f;
+
+    bool jumping = false;
+
+    private Animator animator;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent <Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        animator.SetBool("walking", movementX != 0f);
 
     }
 
@@ -26,27 +34,51 @@ public class Player : MonoBehaviour
         Vector2 v = value.Get<Vector2>();
 
         movementX = v.x;
-        movementY = v.y;
+        movementY = v.y; //In 2D we don't need to worry about Y
 
         Debug.Log(v);
     }
 
+    void OnJump()
+    {
+        if (touchingGround)
+        {
+            jumping = true;
+        }
+    }
     void FixedUpdate()
     {
-        float XmoveDistance = movementX * speed * Time.fixedDeltaTime;
-        float YmoveDistance = movementY * speed * Time.fixedDeltaTime;
+        float XmoveDistance = movementX * speed;
+        //float YmoveDistance = movementY * speed;
 
-        transform.position = new Vector2(transform.position.x + XmoveDistance, transform.position.y + YmoveDistance);
+        //transform.position = new Vector2(transform.position.x + XmoveDistance, transform.position.y + YmoveDistance);
 
-        // rb.LinearVelocity = new Vector2(XmoveDistance, YmoveDistance);
+        rb.linearVelocityX = XmoveDistance;
+        //rb.linearVelocityY = movementY * 10f;
+        //rb.linearVelocity = new Vector2(movementX, movementY).normalized;
+
+        if (touchingGround && jumping)
+        {
+            rb.AddForce(jumpPower * Vector2.up, ForceMode2D.Impulse);
+            jumping = false;
+        }
+
     }
-    
+    bool touchingGround;
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        // if (collision.gameObject.CompareTag("ground"))
-        // {
-        //     rb.AddForce(new Vector2(Random.Range(-150, 200), 500));
-        // }
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            touchingGround = true;
+        }
     }
+    private void ConCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            touchingGround = false;
+        }
+    }
+    
 
 }
